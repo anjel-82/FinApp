c54dd028b942f6ae794856ceb94a1c1fd6cb7e6b
amendizabal <>
2024-12-10 21:56:06 +0100

    Second commit

--- finapp.py
+++ finapp.py
@@ -1,3 +1,15 @@
 import lib.libgoogle as libgoogle
 
-libgoogle.download_excel_from_gdrive()
+#Download Fondos.xlsx from GoogleDrive
+libgoogle.download_excel_from_gdrive('Fondos')
+
+#Añadir datos en Diciembre en la hoja de Anjel y Maitane
+import pandas as pd
+df_anjel = pd.read_excel('Fondos.xlsx',sheet_name='Anjel')
+df_maitane = pd.read_excel('Fondos.xlsx',sheet_name='Maitane')
+
+index = df_anjel[df_anjel['Date'] == '/12/2024'].index.tolist() 
+df_anjel.loc[13,'IE00B03HCZ61'] = 666
+df_anjel.to_excel('Fondos_copia.xlsx')
+
+#libgoogle.upload_excel_to_gdrive_and_remove('Fondos')
--- lib/libgoogle.py
+++ lib/libgoogle.py
@@ -1,12 +1,41 @@
-def download_excel_from_gdrive():
-    from pydrive2.auth import GoogleAuth
-    from pydrive2.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import gdown as gd
+import datetime
+import os
 
-    import gdown as gd
+def download_excel_from_gdrive(googleFile):
 
     #Descarga de ExcelCloud.xlsx
     #Es necesario que el fichero a descargar estÃ© compartido para TODOS los que tengan link
-    print('***** Download Fondox.xlsx')
+    print('***** Download ' +googleFile+'.xlsx')
     url = 'https://docs.google.com/spreadsheets/d/1q1pTRNJOd4a-tdwLi1UXmuqMi15GCB3B/edit?usp=sharing&ouid=102456934783095133027&rtpof=true&sd=true'
-    googleFile = 'Fondos'
     gd.download(url, googleFile+'.xlsx', quiet=False,fuzzy=True)
+
+def upload_excel_to_gdrive_and_remove(googleFile):
+    #Añadir fecha al final del nombre del fichero
+
+    # Obtener la fecha y hora actual 
+    now = datetime.datetime.now()
+    # Formatear la fecha y hora como una cadena 
+    timestamp = now.strftime("%Y%m%d_%H%M%S")
+
+
+    # Autenticación
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()  # Esto abrirá una ventana del navegador para autenticarse
+    drive = GoogleDrive(gauth)
+
+    # Crear y subir el archivo
+    folder_id = '11x5pymyWpBsXSH_07gKPpxl2YjTxB0xA'
+    OutFile = googleFile + '_' + timestamp + '.xlsx'
+    print('Uploading..... ' + OutFile + ' file to Google Drive')
+    file = drive.CreateFile({'title': OutFile, 'parents': [{'id': folder_id}]}) # Cambia 'nombre_del_archivo.xlsx' por el nombre que quieras darle al archivo en Google Drive
+    file.SetContentFile(googleFile+'.xlsx')  # Cambia 'ruta_al_archivo.xlsx' por la ruta al archivo Excel en tu sistema
+    file.Upload()
+    print('File uploaded')
+
+    #Eliminar fichero local.xlsx
+    # Cerrar el archivo antes de eliminarlo 
+    file = None
+    os.remove(googleFile+'.xlsx')
